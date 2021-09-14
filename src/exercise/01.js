5fe03494484bea8d1e3fc39d1243eff4cdb9e2db
AntwanSherif <antwansherif@gmail.com>
2021-09-14 22:43:58 +0200

    :sparkles: Implement 01. Set an initial value (Extra credit 1)

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,8 +3,8 @@
 
 import * as React from 'react'
 
-function Greeting() {
-  const [name, setName] = React.useState('')
+function Greeting({initialName = ''}) {
+  const [name, setName] = React.useState(initialName)
 
   function handleChange(event) {
     setName(event.target.value)
@@ -22,7 +22,7 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Antwan" />
 }
 
 export default App
